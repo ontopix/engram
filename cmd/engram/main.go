cc8a0d49708dea806eab72d5cbc047acd1bf73cc
Albert Puigsech Galicia <albert@puigsech.com>
2026-08-09 16:11:43 +0200

    feat: wire local workflow commands

--- cmd/engram/main.go
+++ cmd/engram/main.go
@@ -10,6 +10,13 @@ import (
 
 func main() {
 	app := cli.NewApp()
+	app.Stdin = os.Stdin
 	commands.RegisterPortable(app)
+	commands.RegisterManagedReads(app)
+	commands.RegisterAttachments(app)
+	commands.RegisterStaging(app)
+	commands.RegisterDraft(app)
+	commands.RegisterHooks(app)
+	commands.RegisterAcquisition(app)
 	os.Exit(app.Run(context.Background(), os.Args[1:], os.Stdout, os.Stderr))
 }
